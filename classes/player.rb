e05a41bec4446ee4c2640d35464e17acde144c9c
Tim Anfilofieff <39632133+Jauq@users.noreply.github.com>
2019-12-13 12:33:04 -0800

    player update
    Added some potential stats to the player class.

--- classes/player.rb
+++ classes/player.rb
@@ -1,7 +1,13 @@
 class Player
-  attr_accessor :name
+  attr_accessor :name, :mhp, :chp, :malp, :base, :atk, :def
   def initialize
     @name = "Yakov"
+    @mhp = 20
+    @chp = 20
+    @malp = 12
+    @base = [4, 4, 4]
+    @atk = [0, 0, 0]
+    @def = [0, 0, 0]
   end
 
 end
